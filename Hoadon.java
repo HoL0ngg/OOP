08eeeefa51c1bc5c072155154fb1d475766847af
HoL0ngg <85066055+HoL0ngg@users.noreply.github.com>
2024-05-16 14:04:19 +0700

    them linh tinh

--- Hoadon.java
+++ Hoadon.java
@@ -6,10 +6,10 @@ public class Hoadon {
     public NgayThang ngayHoadon;
     public int tienHoadon;
 
-    public static Scanner inp = new Scanner(System.in);
     public Hoadon() {
 
     }
+
     public Hoadon(String maHoadon, String maNhanvien, NgayThang ngayHoadon, int tienHoadon) {
         this.maHoadon = maHoadon;
         this.maNhanvien = maNhanvien;
@@ -33,7 +33,6 @@ public class Hoadon {
         return maHoadon;
     }
 
-
     public int getTienHoadon() {
         return tienHoadon;
     }
@@ -50,29 +49,29 @@ public class Hoadon {
         this.ngayHoadon = ngayHoadon;
     }
 
-    public void xuatHoadon () {
+    public void xuatHoadon() {
         System.out.println("+=========================================+");
         System.out.println("|                  HOA DON                |");
         System.out.println("+-----------------------------------------+");
-        // System.out.println("| Ma hoa don: " + getMaHoadon() + "                       |");
-        // System.out.println("| Ma nhan vien: " + getMaNhanvien() + "                     |");
-        // System.out.println("| Ngay: " + getNgayHoadon() + "                          |");
-        // System.out.println("| Tong tien: " + getTienHoadon() + "                       |");
-        System.out.format("%-41s %-1s", 
-        "|  Ma hoa don: " + this.getMaHoadon(),
-        "|");
+        // System.out.println("| Ma hoa don: " + getMaHoadon() + " |");
+        // System.out.println("| Ma nhan vien: " + getMaNhanvien() + " |");
+        // System.out.println("| Ngay: " + getNgayHoadon() + " |");
+        // System.out.println("| Tong tien: " + getTienHoadon() + " |");
+        System.out.format("%-41s %-1s",
+                "|  Ma hoa don: " + this.getMaHoadon(),
+                "|");
         System.out.println();
-        System.out.format("%-41s %-1s", 
-        "|  Ma nhan vien: " + this.getMaNhanvien(),
-        "|");
+        System.out.format("%-41s %-1s",
+                "|  Ma nhan vien: " + this.getMaNhanvien(),
+                "|");
         System.out.println();
-        System.out.format("%-41s %-1s", 
-        "|  Ngay: " + this.getNgayHoadon(),
-        "|");
+        System.out.format("%-41s %-1s",
+                "|  Ngay: " + this.getNgayHoadon(),
+                "|");
         System.out.println();
-        System.out.format("%-41s %-1s", 
-        "|  Tong tien: " + this.getTienHoadon(),
-        "|");
+        System.out.format("%-41s %-1s",
+                "|  Tong tien: " + this.getTienHoadon(),
+                "|");
         System.out.println();
         System.out.println("+-----------------------------------------+");
     }
