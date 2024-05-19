a73381e8da7485f8b6e82609befc3a3089ed7865
HoL0ngg <85066055+HoL0ngg@users.noreply.github.com>
2024-05-19 15:27:11 +0700

    so so phan ThongKe

--- HOADON/ThongKe.java
+++ HOADON/ThongKe.java
@@ -1,7 +1,6 @@
 package HOADON;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import CHUCNANG.ChucNang;
@@ -15,31 +14,37 @@ public class ThongKe {
     public ThongKe() {
     }
 
-    public static void thongKeThang() {
+    public static void thongKeQuy() {
         ThongKe.soluongdon = new int[4];
         ThongKe.doanhthu = new int[4];
         ThongKe.soluonghang = new int[ThucDon.thucdon.size()];
-        Calendar calendar = Calendar.getInstance();
+        System.out.println("Nhap nam muon thong ke ");
+        int nam = ChucNang.chuanHoa(2024);
         for (Hoadon hd : DSHoaDon.getDSHD()) {
-            soluongdon[hd.getNgayHoadon().getNam() / 4]++;
+            if (hd.getNgayHoadon().getNam() == nam) {
+                soluongdon[hd.getNgayHoadon().getNam() % 4]++;
+                doanhthu[hd.getNgayHoadon().getNam() % 4] += hd.getTienHoadon();
+            }
         }
-        for (CTHD cthd : DSHoaDon.getCTHDList()) {
-
-        }
-        System.out.println("+=======================================================+");
-        System.out.println("|                     THONG KE QUY                      |");
-        System.out.println("+-------------------------------------------------------+");
-        System.out.println("|    QUY I    |    QUY II   |   QUY  III  |   QUY  IV   |");
-        System.out.println("+-------------------------------------------------------+");
+        System.out.println("+===============================================================================+");
+        System.out.println("|                                 THONG KE QUY                                  |");
+        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.println("|     LOAI      |     QUY I     |     QUY II    |    QUY  III   |    QUY  IV    |");
+        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.print("| So luong don\t");
         for (int i = 0; i < 4; ++i) {
-            System.out.printf("|%-13d don", soluongdon[i]);
+            System.out.printf("|       %-8s", soluongdon[i]);
         }
         System.out.println("|");
+        System.out.print("| Tong tien\t");
         for (int i = 0; i < 4; ++i) {
-            System.out.printf("|%-13sd", ChucNang.chuanHoaGia(doanhthu[i]));
+            if (doanhthu[i] == 0) {
+                System.out.printf("|       %-8d", 0);
+            } else
+                System.out.printf("|     %-10s", ChucNang.chuanHoaGia(doanhthu[i]));
         }
         System.out.println("|");
-        System.out.println("+=======================================================+");
+        System.out.println("+===============================================================================+");
     }
 
     public static void thongKeNgay() {
@@ -48,9 +53,36 @@ public class ThongKe {
         ThongKe.soluonghang = new int[ThucDon.thucdon.size()];
     }
 
-    public static void thongKeNam() {
-        ThongKe.soluongdon = new int[4];
-        ThongKe.doanhthu = new int[4];
+    public static void thongKeThang() {
+        ThongKe.soluongdon = new int[12];
+        ThongKe.doanhthu = new int[12];
         ThongKe.soluonghang = new int[ThucDon.thucdon.size()];
+        System.out.println("Nhap nam muon thong ke ");
+        int nam = ChucNang.chuanHoa(2024);
+        for (Hoadon hd : DSHoaDon.getDSHD()) {
+            if (hd.getNgayHoadon().getNam() == nam) {
+                soluongdon[hd.getNgayHoadon().getThang() % 12]++;
+                doanhthu[hd.getNgayHoadon().getThang() % 12] += hd.getTienHoadon();
+            }
+        }
+        System.out.println("+===============================================================================+");
+        System.out.println("|                                 THONG KE QUY                                  |");
+        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.println("|     LOAI      |     QUY I     |     QUY II    |    QUY  III   |    QUY  IV    |");
+        System.out.println("+-------------------------------------------------------------------------------+");
+        System.out.print("| So luong don\t");
+        for (int i = 0; i < 4; ++i) {
+            System.out.printf("|       %-8s", soluongdon[i]);
+        }
+        System.out.println("|");
+        System.out.print("| Tong tien\t");
+        for (int i = 0; i < 4; ++i) {
+            if (doanhthu[i] == 0) {
+                System.out.printf("|       %-8d", 0);
+            } else
+                System.out.printf("|     %-10s", ChucNang.chuanHoaGia(doanhthu[i]));
+        }
+        System.out.println("|");
+        System.out.println("+===============================================================================+");
     }
 }
